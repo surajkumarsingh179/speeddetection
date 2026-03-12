b1be3d80b75ecc9fa845c45d6a19b4bfe6ab2654
Pushkar2317-byte <singhpushkar03792@gmail.com>
2026-03-12 10:32:41 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -2,7 +2,7 @@
 #include <LiquidCrystal_I2C.h>
 
 // ---------------- LCD ----------------
-LiquidCrystal_I2C lcd(0x27, 16, 2);   // change to 0x3F if needed
+LiquidCrystal_I2C lcd(0x27, 16, 2);  
 
 // ------------- IR Sensors -------------
 #define SENSOR_1_PIN 2   // INT0
@@ -83,3 +83,4 @@ void loop() {
     delay(1500);
   }
 }
+
